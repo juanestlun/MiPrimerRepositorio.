3e42f1128c4410fe53ca6d4f757866093e6d6bd5
juanestlun <juanpabloestrellaluna177@gmail.com>
2026-07-18 16:14:35 -0600

    Se agregó validación de datos.py

--- miprimerprograma.py
+++ miprimerprograma.py
@@ -1,3 +1,7 @@
 nombre = input("Ingresa tu nombre: ")
-edad = input("Ingresa tu edad: ")
+while True:
+    edad = input("Ingresa tu edad: ")
+    if edad.isdigit():
+        break
+    print("Error: Por favor, ingresa un número válido para la edad.")
 print(f"Hola {nombre}, tienes {edad} años.")
